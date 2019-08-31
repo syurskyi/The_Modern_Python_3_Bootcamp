34ae517a9d8186b824ebec06539df578e395ee99
Grey <sergejyurskyj@yahoo.com>
2019-08-31 12:06:14 -0700

    Commit Section 7: Coding Exercise

---
+++ Section 7 Variables and Strings/Coding Exercise 4 Make Some Variables!.py
@@ -0,0 +1,12 @@
+# Define a variable named city and set it equal to any string
+city = "Los Angeles"
+
+# Define a variable named price and set it equal to any float
+price = 5.4
+
+# Define a variable named high_score and set it equal to any int
+high_score = 100002
+
+# Define a variable named is_having_fun and set it to a Boolean value
+is_having_fun = True
+
